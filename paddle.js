26cec838ce7e8319caa57056bc10ea9609aa8d8a
RNeneJTech <104571066+RNeneJTech@users.noreply.github.com>
2022-05-05 15:32:38 +0300

    h

--- paddle.js
+++ paddle.js
@@ -4,17 +4,17 @@ class paddle
     {
         this.width = pwidth;
         this.height = pheight;
-        this.xcenter = xcenter;
-        this.ycenter = ycenter;
+        this.xcenter = xcenter - pwidth / 2;
+        this.ycenter = ycenter - pheight / 2;
     }
 
-    get get_x_center() { return this.xcenter;}
-    get get_y_center() { return this.ycenter;}
-    set set_x_center(xcenter) { return this.xcenter;}
-    set set_y_center(ycenter) { return this.ycenter;}
+    get get_x_center() { return this.xcenter - pwidth / 2;}
+    get get_y_center() { return this.ycenter - pheight / 2;}
+    set set_x_center(xcenter) { return this.xcenter - this.width / 2;}
+    set set_y_center(ycenter) { return this.ycenter - this.height / 2;}
 
-    get get_width() { return this.xcenter;}
-    get get_height() { return this.ycenter;}
+    get get_width() { return this.width;}
+    get get_height() { return this.height;}
     set set_width(pwidth) { this.width = pwidth;}
     set set_height(pheight) { this.height = pheight;}
 
@@ -28,6 +28,6 @@ class paddle
 
     draw()
     {
-        rect(this.xcenter, this.ycenter, this.width, this.width);
+        rect(this.xcenter, this.ycenter, this.width, this.height);
     }
 }
